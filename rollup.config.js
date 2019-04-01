a9e8f79811835737907cb2b187aad19e861fd71a
Pedro Abreu <pmig.abreu@gmail.com>
2019-04-01 23:08:55 +0100

    Use rollup-plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,48 @@
+import resolve from 'rollup-plugin-node-resolve'
+
 module.exports = [
     {
         input: 'src/index.js',
         output: {
             file: 'lib/index.js',
             format: 'esm'
-        }
+        },
+        plugins: [
+            resolve({
+                module: true
+            })
+        ]
     },
     {
         input: 'src/index.js',
         output: {
             file: 'lib/cjs/index.js',
             format: 'cjs'
-        }
+        },
+        plugins: [
+            resolve()
+        ]
     },
     {
         input: 'src/locale/index.js',
         output: {
             file: 'lib/cjs/locale.js',
             format: 'cjs'
-        }
+        },
+        plugins: [
+            resolve()
+        ]
     },
     {
         input: 'src/locale/index.js',
         output: {
             file: 'lib/locale.js',
             format: 'esm'
-        }
+        },
+        plugins: [
+            resolve({
+                module: true
+            })
+        ]
     }
 ]
